f1962855eb4468d4999d142d14ef420c661dc25e
SoWar <junky.man@yandex.ru>
2017-05-11 18:58:28 +0300

    Оживление социальных (нужны новые изображения)

---
+++ js/script.js
@@ -0,0 +1,3 @@
+var form = document.querySelector(".form-block");
+var form_button = document.querySelector(".search-form-trigger");
+form_button.addEventListener("click", function(){form.classList.toggle("display-toggle")})
