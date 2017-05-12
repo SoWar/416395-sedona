1b1e36e03e515f13d34d389276f87207a02def85
SoWar <junky.man@yandex.ru>
2017-05-12 18:20:00 +0300

    Animation fail
    Анимация добавлена, но не понятно как сделать заезд под форму.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 var form = document.querySelector(".form-block");
 var form_button = document.querySelector(".search-form-trigger");
-form_button.addEventListener("click", function(){form.classList.toggle("display-toggle")})
+form_button.addEventListener("click", function(){
+
+// if (form.className.match("display-toggle")) {
+//   console.log("yes");
+//   form.classList.remove("slide-down");
+//   form.classList.add("slide-up");
+// } else {
+//   console.log("no");
+//   form.classList.remove("slide-up");
+//   form.classList.add("slide-down");
+// }
+
+form.classList.toggle("display-toggle");
+
+})
