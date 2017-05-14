7000b79be427a16df783b9f50134c704efff5a34
SoWar <junky.man@yandex.ru>
2017-05-14 22:53:03 +0300

    Attempt2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 var form = document.querySelector(".form-block");
 var form_button = document.querySelector(".search-form-trigger");
+form.classList.toggle("display-toggle");
 form_button.addEventListener("click", function(){form.classList.toggle("display-toggle")})
